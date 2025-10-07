37cfd122fbf98fab829ec4bc18fb64a079868f68
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2025-10-07 12:15:26 -0400

    commented out steps per instructions as well as initailizing the array the final, point and end of array.

--- main.c
+++ main.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
 
 int main (){
+    int finalExams[10] = {0};        // Step 1: Create and initialize array of size 10
+    int *startPtr = finalExams;      // Points to start of array
+    int *endPtr = finalExams + 10;   // Points to one past the end of array
+    int *ptr;  
+
+
+
+    // Step 2: Update first 5 elements using only pointers
+
+
+
+
+    // Step 3: Add two additional scores (using pointers only)
+
+    // Step 4: Boost any score between 60–69 to 70
+
+        /*
+        
+    QUESTIONS
+
+    1) If you had sized the finalExams array based on your class size (5), 
+       what just happened when you added scores for two additional students?
+
+    2) If you executed: testPtr = startPtr - endPtr and then tried to store 
+       the value "5" in location testPtr, what would happen?
+
+    */
+
+
+
+
+
+
 
     return 0;
 }
